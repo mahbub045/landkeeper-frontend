4f163f009f1eb346b3b14b68c5ec66d21de43138
mahbub045 <mahbubrmn45@gmail.com>
2026-06-17 10:30:12 +0600

    Hide logo when side bar collaps

--- components/layout/AppSidebar.tsx
+++ components/layout/AppSidebar.tsx
@@ -97,7 +97,7 @@ export default function AppSidebar() {
   return (
     <Sidebar collapsible="icon">
       <SidebarHeader className="gap-0 px-4 py-4 group-data-[collapsible=icon]:px-2">
-        <div className="flex items-center gap-2 group-data-[collapsible=icon]:justify-center">
+        <div className="flex items-center gap-2 group-data-[collapsible=icon]:hidden">
           <Image
             src={logoSrc}
             alt="Landkeeper"
