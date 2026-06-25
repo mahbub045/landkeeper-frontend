56263c406a4348db1417175fde9a389bbc142e25
mahbub045 <mahbubrmn45@gmail.com>
2026-06-25 17:37:44 +0600

    Change place holder

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -114,7 +114,7 @@ export default function ForgotPasswordPage() {
                 <Input
                   id='email'
                   type='email'
-                  placeholder='you@organisation.com'
+                  placeholder='you@example.com'
                   value={email}
                   onChange={(e) => {
                     setEmail(e.target.value);
