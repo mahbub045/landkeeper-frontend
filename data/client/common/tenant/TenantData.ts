9124c4cd23a9b1d8c96d6bb6b181949a21ede8fd
Md Mahbub Rahman <mahbubrmn45@gmail.com>
2026-07-16 10:56:57 +0600

    Fix epmty form field

--- data/client/common/tenant/TenantData.ts
+++ data/client/common/tenant/TenantData.ts
@@ -11,7 +11,9 @@ export const avatarColors = [
 
 export const EMPTY_FORM: TenantForm = {
   propertyId: '',
+  title: '',
   firstName: '',
+  middleName: '',
   lastName: '',
   email: '',
   phone: '',
